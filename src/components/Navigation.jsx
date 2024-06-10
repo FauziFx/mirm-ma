4372627b26415296942b56ef4ea9749291dd4c47
Ahmad Fauzi <fauzitkj3@gmail.com>
2024-06-11 06:53:25 +0700

    feat: add signout on navigation and underline navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useRef } from "react";
 import { Outlet, Link } from "react-router-dom";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import Footer from "./Footer";
+import Avatar from "react-avatar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import Cookies from "universal-cookie";
 
 function Navigation() {
+  const cookies = new Cookies();
   const toggleRef = useRef(null);
   const [menu, setMenu] = useState([
     {
@@ -19,7 +24,7 @@ function Navigation() {
       link: "/rekam-medis",
     },
     {
-      nama: "garansi",
+      nama: "Garansi",
       link: "/garansi",
     },
     {
@@ -27,6 +32,14 @@ function Navigation() {
       link: "/data-optik",
     },
   ]);
+
+  const signOut = () => {
+    cookies.remove("rm-ma-token");
+    setTimeout(() => {
+      window.location.replace("/login");
+    }, 500);
+  };
+
   return (
     <>
       <Navbar
@@ -37,7 +50,7 @@ function Navigation() {
         fixed="top"
       >
         <Container>
-          <Navbar.Brand href="" className="fw-bold">
+          <Navbar.Brand href="/" className="fw-bold">
             Murti Aji
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" ref={toggleRef} />
@@ -55,9 +68,70 @@ function Navigation() {
                 </Link>
               ))}
             </Nav>
+            <Nav>
+              <NavDropdown
+                title={
+                  <>
+                    <Avatar
+                      name="Admin Ganteng"
+                      size={30}
+                      round
+                      className="me-1"
+                    />
+                    Admin Ganteng
+                  </>
+                }
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item href="#action/3.1">
+                  <Avatar
+                    name="Admin Ganteng"
+                    size={30}
+                    round
+                    className="me-1"
+                  />
+                  Admin Ganteng
+                </NavDropdown.Item>
+                <NavDropdown.Item disabled>Admin@Ganteng.com</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item
+                  href="#action/3.4"
+                  className="text-center"
+                  onClick={() => signOut()}
+                >
+                  <FontAwesomeIcon icon={faRightFromBracket} className="me-1" />
+                  Keluar
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      <Container className="d-block d-sm-none">
+        <div className="nav-scroller bg-body shadow-sm">
+          <Nav className="me-auto">
+            {menu.map((item, index) => (
+              <Link
+                key={index}
+                to={item.link}
+                type="button"
+                className="nav-link"
+              >
+                {item.nama}
+              </Link>
+            ))}
+            <Link
+              type="button"
+              className="nav-link ms-4"
+              onClick={() => signOut()}
+            >
+              <FontAwesomeIcon icon={faRightFromBracket} className="me-1" />
+              Keluar
+            </Link>
+          </Nav>
+        </div>
+      </Container>
 
       <Outlet />
       <Footer />
